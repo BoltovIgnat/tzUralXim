a9c571f353591a6c277660e7ef78d2ae37b80f0d
Петр Пивоваров <pivovarov@buroburo.ru>
2022-12-09 14:56:43 +0300

    init

--- local/modules/ibc.jwt/assets/routes/jwt-api.php
+++ local/modules/ibc.jwt/assets/routes/jwt-api.php
@@ -55,17 +55,98 @@ return function (RoutingConfigurator $routes) {
     });
 
     $routes->any('/local/rest/auth', function () {
-        // сработает на любой тип запроса
-        return '2)	/local/rest/auth';
+        $request = \Bitrix\Main\Context::getCurrent()->getRequest();
+        $params = $request->getPostList()->toArray(); // массив post параметров
+        global $USER;
+        if (!is_object($USER)) $USER = new CUser;
+        $arAuthResult = $USER->Login($params['LOGIN'], $params['PASSWORD'], "Y");
+        if ($arAuthResult){
+            $jwtkey = IbcJwtHelper::getJwtKeyByUserId($USER->GetID());
+            return json_encode([
+                'result' => "Пользователь успешно авторизовался.",
+                'jwtkey' => $jwtkey
+            ]);
+        }else{
+            return json_encode([
+                'result' => 'error',
+                'msg' =>  $arAuthResult
+            ]);
+        }
+
+
     });
 
-    $routes->any('/local/rest/users/{id}', function () {
-        // сработает на любой тип запроса
-        return '3)	/local/rest/users/{id}';
+    $routes->any('/local/rest/users/{id}', function ($id) {
+        $request = \Bitrix\Main\Context::getCurrent()->getRequest();
+        $params = $request->getPostList()->toArray();
+
+        if (IbcJwtHelper::valideteJwtKey($params['jwtkey'])){
+
+            $rsUser = CUser::GetByID($id);
+            $arUser = $rsUser->Fetch();
+            $arUser['USER_GROUPS'] = CUser::GetUserGroup($id);
+
+            return json_encode([
+                'result' => "Даные по пользователю.",
+                'data' => $arUser
+            ]);
+        }else{
+            return json_encode([
+                'result' => 'error',
+                'msg' =>  'Указан не корректный ключ'
+            ]);
+        }
     });
 
     $routes->any('/local/rest/users/', function () {
-        // сработает на любой тип запроса
-        return '4)	/local/rest/users/';
+        $request = \Bitrix\Main\Context::getCurrent()->getRequest();
+        $params = $request->getPostList()->toArray();
+
+        if (IbcJwtHelper::valideteJwtKey($params['jwtkey'])){
+
+            $cache = \Bitrix\Main\Application::getInstance()->getManagedCache();
+            $cacheId = 'userpageid'.$params['PAGE'];
+            $arResult = [];
+
+            if ($cache->read(7200, $cacheId)) {
+                $arResult = $cache->get($cacheId); // достаем переменные из кеша
+            } else {
+
+                $result = \Bitrix\Main\UserTable::getList(array(
+
+                    'select' => array('*'), // выберем идентификатор и генерируемое (expression) поле SHORT_NAME
+
+                    'order' => array(), // все группы, кроме основной группы администраторов,
+
+                    'limit'   => 10,
+
+                    'offset' => $params['PAGE']*10
+
+                ));
+
+
+
+                while ($arUser = $result->fetch()) {
+
+                    $arResult[$arUser['ID']] = $arUser;
+                    $arResult[$arUser['ID']]['USER_GROUPS'] = \Bitrix\Main\UserTable::getUserGroupIds($arUser['ID']);
+
+                }
+
+                $cache->set($cacheId, array("key" => $arResult)); // записываем в кеш
+            }
+
+
+            return json_encode([
+                'result' => "Даные по пользователю.",
+                'data' => $arResult
+            ]);
+
+        }else{
+            return json_encode([
+                'result' => 'error',
+                'msg' =>  'Указан не корректный ключ'
+            ]);
+        }
     });
 };
